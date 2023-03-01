a142523c576e3b114d3f0af6a2c65f87b27356f9
mahaxworkz <126585913+mahaxworkz@users.noreply.github.com>
2023-03-01 17:23:07 +0000

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,27 @@
+public class Task{
+
+    public static void main(String[] args){
+
+    Hard();
+	Easy();
+	Task2.Satisfication();
+    }
+    public static void Hard(){
+
+       System.out.println(" it is hard to complete");
+	  
+    }
+	 public static void Easy(){
+
+       System.out.println("I like it because it easy");
+	  
+    }
+    public class Task2{
+		public static void Satisfication(){
+
+       System.out.println(" it is satisfied");
+	  
+        }
+		
+	}
+}

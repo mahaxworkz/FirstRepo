c459019e3b0043f042522fe722f4189bc9a3a7b6
mahaxworkz <126585913+mahaxworkz@users.noreply.github.com>
2023-03-07 15:59:21 +0000

    Add files via upload

---
+++ Mystring.java
@@ -0,0 +1,46 @@
+public class Mystring{
+    public static void main(String[] args){
+	
+	String city ="Tokyo";
+	String anime ="One punch";
+	String anime_character ="saitama";
+	String song ="Grateful";
+	String singer ="Eminem";
+	String mount ="fuji";
+	String animal ="Black panther";
+	String bird ="hawk";
+	String marvel ="Black panther";
+	String name ="fuji";
+	String person ="Eminem";
+	String area ="saitama";
+	String album ="Grateful";
+	String food ="salmon";
+	
+	
+	String vegetable = new String("Beetroot");
+	String nut = new String("almond");
+	String fruit = new String("orange");
+	String fish= new String("salmon");
+	String greens = new String("spinach");
+	String diet = new String("spinach");
+	String river = new String("ganges");
+	String area_japan = new String("Ottomi");
+	String colour = new String("orange");
+	String degree = new String("engineering");
+	String King = new String("salmon");
+	
+	System.out.println(mount == name && person == singer);
+	System.out.println(album == song );
+	System.out.println(person == singer);
+	System.out.println(area == anime_character);
+	System.out.println(animal == marvel);
+	System.out.println(colour == fruit);
+	System.out.println(greens == diet);
+	System.out.println(colour != fruit);
+	System.out.println(colour == fruit);
+	System.out.println(King == fish);
+	System.out.println(food== fish);
+	}
+
+
+}

4bab26f2631405a84f25ae18c7512f40cebfdc5b
unknown <mahalakshmi.xworkz@gmail.com>
2023-03-23 19:49:18 +0000

    state&behaviour

---
+++ Professor.java
@@ -0,0 +1,59 @@
+public class Professor{
+
+  final String profsessor_name="Praveen";
+  final int ID_number=713602;
+  final long phonenumber= 3456287986L;
+  final byte age =30;
+  final String father_name="Balan";
+  static String email;
+  static String area_of_expertise;
+  static String degree;
+  static String position;
+  static String working_college;
+  int no_of_subjects;
+  String Subject_taken;
+  int Salary;
+  byte year_of_experience;
+  String marital_status;
+  byte periods_taken_per_day;
+  static String LanguageKnown;
+  boolean tution_taken;
+  String previous_working_college;
+  boolean Technical_knowledge;
+  
+
+public static void main(String[] args){
+	
+}
+public void take_lessons(){
+	
+}
+public void completing_potions(){
+	
+}
+public void conduct_test(){
+	
+}
+public void paper_correction(){
+	
+}
+public void conduct_meeting(){
+	
+}
+public void attend_meeting(){
+	
+}
+public void mark_validation(){
+	
+}
+public void external_work(){
+	
+}
+public void internal_work(){
+	
+}
+public void monitoring_disciplinary(){
+	
+}
+
+}
